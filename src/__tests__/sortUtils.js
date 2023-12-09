bb30948c023864b7224e813d0b12212315c863c8
Igor Kirillov <Igor.Kirillov@x5.ru>
2023-12-09 22:11:40 +0300

    tests

--- src/__tests__/sortUtils.js
+++ src/__tests__/sortUtils.js
@@ -1,7 +1,7 @@
 import orderByProps from '../sortUtils';
 
 describe('sort utils module', () => {
-  test ('default case', () => {
+  test ('default case 1', () => {
     const person = {
       name: "Igor",
       surname: "Kirillov",
@@ -12,6 +12,22 @@ describe('sort utils module', () => {
     expect(orderByProps(person, ["name", "age"])).toEqual([
       { key: "name", value: "Igor" },
       { key: "age", value: 37 },
+      { key: "gender", value: "male" },
+      { key: "nickname", value: "laba" },
+      { key: "surname", value: "Kirillov" }
+    ]);
+  });
+  test ('default case 2', () => {
+    const person = {
+      name: "Igor",
+      surname: "Kirillov",
+      nickname: "laba",
+      age: 37,
+      gender: "male"
+    }
+    expect(orderByProps(person, ["age", "name"])).toEqual([
+      { key: "age", value: 37 },
+      { key: "name", value: "Igor" },
       { key: "gender", value: "male" },
       { key: "nickname", value: "laba" },
       { key: "surname", value: "Kirillov" }
--- src/sortUtils.js
+++ src/sortUtils.js
@@ -1,7 +1,9 @@
 export default function orderByProps(object, props) {
-  let keys = Object.keys(object).sort((a, b) => {
+  const keys = Object.keys(object).sort((a, b) => {
     if (props && props.includes(a) && props.includes(b)) {
-      return compareInt(props.indexOf(a), props.indexOf(b));
+      const aIndex = props.indexOf(a);
+      const bIndex = props.indexOf(b);
+      return aIndex < bIndex ? -1 : 1;
     } else if (props && props.includes(a)) {
       return -1;
     } else if (props && props.includes(b)) {
@@ -18,14 +20,4 @@ export default function orderByProps(object, props) {
     });
   }
   return result;
-}
-
-function compareInt(a, b) {
-  if (a === b) {
-    return 0;
-  } else if (a < b) {
-    return -1;
-  } else {
-    return 1;
-  }
 }
